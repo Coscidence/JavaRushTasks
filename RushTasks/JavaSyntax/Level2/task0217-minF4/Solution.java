bffc099811753910d1a613018d7d6a1a3df3b9a8
Alex <Coscidence@users.noreply.github.com>
2020-04-18 10:57:10 +0300

    Min

---
+++ RushTasks/JavaSyntax/Level2/task0217-minF4/Solution.java
@@ -0,0 +1,42 @@
+package com.javarush.task.task02.task0217;
+
+/* 
+Минимум четырех чисел
+*/
+public class Solution {
+    public static int min(int a, int b, int c, int d) {
+        //напишите тут ваш код
+
+        int result = min(a,b);
+                if (result > b)
+        result = b;
+        if (result > c)
+        result = c;
+        if (result > d)
+        result = d;
+        
+        
+        return result;
+
+
+    }
+
+    public static int min(int a, int b) {
+        //напишите тут ваш код
+        int result = a;
+        if (result > b)
+        result = b;
+        
+        return result;
+
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(min(-20, -10));
+        System.out.println(min(-40, -10, -30, 40));
+        System.out.println(min(-20, -40, -30, 40));
+        System.out.println(min(-20, -10, -40, 40));
+        System.out.println(min(-20, -10, -30, -40));
+    }
+}
