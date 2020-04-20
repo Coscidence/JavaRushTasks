30b82a855fee330b89ac853c1ce9eee19b015139
Alex <Coscidence@users.noreply.github.com>
2020-04-20 11:07:53 +0300

    Count factorial

---
+++ RushTasks/JavaSyntax/Level3/0308-factorial/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task03.task0308;
+
+/* 
+Произведение 10 чисел
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int x=10;
+        int fact = 1;
+        for (int i=2; i<=x; i++) {
+            fact = fact * i;
+        }
+        System.out.println(fact);
+
+    }
+}
