b616edc6fabfdbea940d7fe9c56ecd8ceb064d07
Alex <Coscidence@users.noreply.github.com>
2020-04-23 18:31:21 +0300

    Conditional operators: if

---
+++ RushTasks/JavaSyntax/Level4/0414-year/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task04.task0414;
+
+/* 
+Количество дней в году
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(bufferedReader.readLine());
+        if (a % 100 == 0 && a % 400 == 0 || a % 100 != 0 && a % 4 == 0) {
+            System.out.println("количество дней в году: 366");
+        } else {
+            System.out.println("количество дней в году: 365");
+        }
+    }
+}
