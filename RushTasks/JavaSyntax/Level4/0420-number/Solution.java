847b6a1ed11cbe635f6b78cc2b8dff8161d16cc6
Alex <Coscidence@users.noreply.github.com>
2020-04-25 10:54:03 +0300

    Number

---
+++ RushTasks/JavaSyntax/Level4/0420-number/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task04.task0420;
+
+/* 
+Сортировка трех чисел
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int n1 = Integer.parseInt(bufferedReader.readLine());
+        int n2 = Integer.parseInt(bufferedReader.readLine());
+        int n3 = Integer.parseInt(bufferedReader.readLine());
+        if (n1 <= n2 && n2 <= n3) System.out.println(n3 + " " + n2 + " " + n1);
+        else if (n1 <= n3 && n3 <= n2) System.out.println(n2 + " " + n3 + " " + n1);
+        else if (n2 <= n1 && n1 <= n3) System.out.println(n3 + " " + n1 + " " + n2);
+        else if (n2 <= n3 && n3 <= n1) System.out.println(n1 + " " + n3 + " " + n2);
+        else if (n3 <= n1 && n1 <= n2) System.out.println(n2 + " " + n1 + " " + n3);
+        else if (n3 <= n2 && n2 <= n1)System.out.println(n1 + " " + n2 + " " + n3);
+    }
+}
